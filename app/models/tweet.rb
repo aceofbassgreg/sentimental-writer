9c4be21ce77f6f7a707e1f5b6e93e8a71fc3e9f2
Greg <gregory.kenenitz@gmail.com>
2016-01-28 17:45:22 -0500

    validates uniqueness of id_from_twitter

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,4 +5,6 @@ class Tweet < ActiveRecord::Base
   has_many :topics, through: :tweet_topics
   has_many :references
   has_many :people, through: :references
+
+  validates_uniqueness_of :id_from_twitter
 end
